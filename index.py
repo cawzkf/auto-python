4f591bbe6f763fab515d6fbbe99e90f5d7c28737
cawzkf <camilafelixdosreis@gmail.com>
2024-08-25 17:25:49 -0400

    Adição de dados

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
-from openpyxl import workbook, load_workbook
+from openpyxl import Workbook
+from openpyxl.styles import Alignment, Border,Font,Side
 
 def criar_plan(Notas):
-    wb = workbook
-    ws = wb.active
-    ws.title = 'Semestre'
+    wb = Workbook()
+    sheet = wb.active
+    sheet.title = 'Semestres'
     
+    data = [
+        ['Disciplina', 'Ap1','Ap2','Ext','Média Final']
+        ['Física', '-','-','-','-',]
+        ['Poo', '-','-','-','-',]
+        ['Inglês', '-','-','-','-',]
+        ['Extensão', '-','-','-','-',]
+        ['Algoritimos e estrutura de dados', '-','-','-','-',]
+        ['Fundamentos de Fabricação', '-','-','-','-',]
+        ['Sistemas Hidráulicos e pneumáticos', '-','-','-','-',]
+    ]
+
