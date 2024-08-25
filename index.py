c9ea6dcba12db8a3353823a0fe2e87885c7da5ba
cawzkf <camilafelixdosreis@gmail.com>
2024-08-25 17:42:30 -0400

    Definir nome para salvar e chamar func para criar/salvar a planilha

--- index.py
+++ index.py
@@ -1,19 +1,20 @@
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border,Font,Side
 
-def criar_plan(Notas):
+def criar_plan():
+    
     wb = Workbook()
     sheet = wb.active
     sheet.title = 'Semestres'
     
     data = [
-        ['Disciplina', 'Ap1','Ap2','Ext','Média Final']
-        ['Física', '-','-','-','-',]
-        ['Poo', '-','-','-','-',]
-        ['Inglês', '-','-','-','-',]
-        ['Extensão', '-','-','-','-',]
-        ['Algoritimos e estrutura de dados', '-','-','-','-',]
-        ['Fundamentos de Fabricação', '-','-','-','-',]
+        ['Disciplina', 'Ap1','Ap2','Ext','Média Final'],
+        ['Física', '-','-','-','-',],
+        ['Poo', '-','-','-','-',],
+        ['Inglês', '-','-','-','-',],
+        ['Extensão', '-','-','-','-',],
+        ['Algoritimos e estrutura de dados', '-','-','-','-',],
+        ['Fundamentos de Fabricação', '-','-','-','-',],
         ['Sistemas Hidráulicos e pneumáticos', '-','-','-','-',]
     ]
 
@@ -21,3 +22,6 @@ def criar_plan(Notas):
     for row in data:
         sheet.append(row)
         
+    wb.save('Notas.xlsx')
+
+criar_plan()
