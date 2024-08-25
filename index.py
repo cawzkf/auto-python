f98f9ef978a9dce6856ef60fdf07667eaa4052d6
cawzkf <camilafelixdosreis@gmail.com>
2024-08-25 18:51:13 -0400

    Ajuste do conteúdo da célula

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border,Font,Side
+from openpyxl.formatting.rule import CellIsRule
 
 def criar_plan():
     
@@ -9,19 +10,32 @@ def criar_plan():
     
     data = [
         ['Disciplina', 'Ap1','Ap2','Ext','Média Final'],
-        ['Física', '-','-','-','-',],
-        ['Poo', '-','-','-','-',],
-        ['Inglês', '-','-','-','-',],
-        ['Extensão', '-','-','-','-',],
-        ['Algoritimos e estrutura de dados', '-','-','-','-',],
-        ['Fundamentos de Fabricação', '-','-','-','-',],
-        ['Sistemas Hidráulicos e pneumáticos', '-','-','-','-',]
+        ['Física',0,0,0,0],
+        ['Poo', 0,0,0,0],
+        ['Inglês',0,0,0,0],
+        ['Extensão',0,0,0,0],
+        ['Algoritimos e estrutura de dados',0,0,0,0],
+        ['Fundamentos de Fabricação', 0,0,0,0],
+        ['Sistemas Hidráulicos e pneumáticos',0,0,0,0]
     ]
 
-
+    
     for row in data:
         sheet.append(row)
+            
+    for col in sheet.columns:
+        max_length = 0
+        column = col[0].column_letter
+        for cell in col:
+            try:
+                if len(str(cell.value))> max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+            adjusted_width = max_length + 2 
+            sheet.column_dimensions[column].width = adjusted_width
         
+            
     wb.save('Notas.xlsx')
 
 criar_plan()
