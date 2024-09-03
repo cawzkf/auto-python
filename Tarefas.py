d74a76c6d1ad02988251156f84c4867fbb7a7ded
cawzkf <camilafelixdosreis@gmail.com>
2024-09-03 09:51:18 -0400

    Apelido para a função

--- Tarefas.py
+++ Tarefas.py
@@ -1,10 +1,10 @@
-import webbrowser
+import webbrowser as plan
 import os
 
 pagina_web = ("D:\OneDrive\Documentos\Camila\Github\Html e css\modelo_web\index.html")
 
 opera = r"C:\Users\Marcus\AppData\Local\Programs\Opera GX\opera.exe"
 
-webbrowser.register('opera',None,webbrowser.BackgroundBrowser(opera))
+plan.register('opera',None,plan.BackgroundBrowser(opera))
 
-webbrowser.get('opera').open(f"file://{pagina_web}")
+plan.get('opera').open(f"file://{pagina_web}")
